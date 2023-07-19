245f02aceb6b96527414640d97c4514ff95bf9c7
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2023-07-19 21:28:37 +0100

    Fix: (hostinger htaaccess)

--- src/services/transaction.service.js
+++ src/services/transaction.service.js
@@ -14,41 +14,118 @@ import {
 } from "./constants";
 
 const transactionApiSlice = apiSlice.injectEndpoints({
-    endpoints: (builder) => ({
-        // get all transactions
-        allTransactions: builder.query({
-            providesTags: ["Transaction"],
-            query: () => ({
-                url: GET_ALL_TRANSACTIONS,
-                method: "GET",
-            }),
-            transformResponse: (response) => {
-                return response
-            }
-        }),
+  endpoints: (builder) => ({
+    // get all transactions
+    allTransactions: builder.query({
+      providesTags: ["Transaction"],
+      query: () => ({
+        url: GET_ALL_TRANSACTIONS,
+        method: "GET",
+      }),
+      transformResponse: (response) => {
+        return response;
+      },
+    }),
 
-        // get my transaction
-        getMyTransaction: builder.query({
-            providesTags: ["Transaction"],
-            query: () => ({
-                url: GET_MY_TRANSACTIONS,
-                method: "GET",
-            }),
-            transformResponse: (response) => {
-                return response
-            }
-        }),
+    // get my transaction
+    getMyTransaction: builder.query({
+      providesTags: ["Transaction"],
+      query: () => ({
+        url: GET_MY_TRANSACTIONS,
+        method: "GET",
+      }),
+      transformResponse: (response) => {
+        return response;
+      },
+    }),
 
-        // get sell items
-        getSellItems: builder.query({
-            providesTags: ["Transaction"],
-            query: () => ({
-                url: GET_SELL_ITEMS,
-                method: "GET",
-            }),
-            transformResponse: (response) => {
-                return response
-            }
+    // get sell items
+    getSellItems: builder.query({
+      providesTags: ["Transaction"],
+      query: () => ({
+        url: GET_SELL_ITEMS,
+        method: "GET",
+      }),
+      transformResponse: (response) => {
+        return response;
+      },
+    }),
+
+    // get buy items
+    getBuyItems: builder.query({
+      providesTags: ["Transaction"],
+      query: () => ({
+        url: GET_BUY_ITEMS,
+        method: "GET",
+      }),
+      transformResponse: (response) => {
+        return response;
+      },
+    }),
+
+    // initiate buy
+    initiateBuy: builder.mutation({
+      invalidatesTags: ["Transaction"],
+      query: (data) => ({
+        url: INITIATE_BUY,
+        method: "POST",
+        body: data,
+      }),
+      transformResponse: (response) => {
+        return response;
+      },
+    }),
+
+    // set buy order
+    setBuyOrder: builder.mutation({
+        invalidatesTags: ["Transaction"],
+        query: (data) => ({
+            url: SET_BUY_ORDER,
+            method: "POST",
+            body: data,
         })
-    })
-})
+    }),
+
+    // payment made
+    paymentMade: builder.mutation({
+        invalidatesTags: ["Transaction"],
+        query: (data) => ({
+            url: PAYMENT_MADE,
+            method: "POST",
+            body: data,
+        }),
+    }),
+
+    // payment recieved
+    paymentRecieved: builder.mutation({
+        invalidatesTags: ["Transaction"],
+        query: (data) => ({
+            url: PAYMENT_RECIEVED,
+            method: "POST",
+            body: data,
+        }),
+    }),
+
+    // transaction failed
+    transactionFailed: builder.mutation({
+        invalidatesTags: ["Transaction"],
+        query: (data) => ({
+            url: TRANSACTION_FAILED,
+            method: "POST",
+            body: data,
+        })
+    }),
+
+    // transaction success
+    transactionSuccess: builder.mutation({
+        
+    }),
+  }),
+});
+
+export const {
+  useAllTransactionsQuery,
+  useGetBuyItemsQuery,
+  useGetMyTransactionQuery,
+  useGetSellItemsQuery,
+} = transactionApiSlice;
