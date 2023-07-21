fccb3b591a562968f8d5f5e2ea83dfa6bf1eeb92
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2023-07-21 12:40:05 +0100

    Feat: (mobile menu)

--- src/containers/dashboard/dashboard-content/super-admin-dashboard/super-admin-dashboard-home/SuperAdminDashHome.jsx
+++ src/containers/dashboard/dashboard-content/super-admin-dashboard/super-admin-dashboard-home/SuperAdminDashHome.jsx
@@ -4,11 +4,12 @@ import { CarbonCreditChart, SuperAdminStatCard, ChartFilter } from "components";
 import { reg, trans, proj } from "assets/images";
 import { useUnverifiedOrganisationQuery } from "services/organisation.service";
 import { useAllProjectsQuery } from "services/project.service";
-import { projectApi } from "services/project.service";
+import { useAllTransactionsQuery } from "services/transaction.service";
 
 const SuperAdminDashHome = () => {
   const { data: unverifiedData } = useUnverifiedOrganisationQuery();
   const {data: projectData} = useAllProjectsQuery();
+  const {data: transactionData} = useAllTransactionsQuery();
   return (
     <div className="sup_admin_dash_home dash_pad">
       <div className="sup_admin_dash_home_card_wrap between">
@@ -35,6 +36,8 @@ const SuperAdminDashHome = () => {
             name={"Transactions"}
             desc={"Total Transactions in last 24 hrs"}
             img={trans}
+            link="/dashboard-wallet"
+            data={transactionData?.length}
           />
         </div>
       </div>
