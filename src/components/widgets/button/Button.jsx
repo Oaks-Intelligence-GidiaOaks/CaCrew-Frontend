88a22bcb9bcf33ecfa90a36e137d94b7928f9e56
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2023-08-04 16:16:53 +0100

    Feat: send/recieve message

---
+++ src/components/widgets/button/Button.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Button.scss";
+import { ThreeDots } from "react-loader-spinner";
+
+const Button = ({ text, loading, disabled, className, type, loadColor, ...rest }) => {
+  return (
+    <button
+      {...rest}
+      type={type}
+      disabled={disabled}
+      className={` ${disabled ? "invalid" : "button"} ${className} center `}
+      // style={{ cursor: "not-allowed" }}
+    >
+      {loading ? (
+        <ThreeDots
+          height="15"
+          width="15"
+          radius="9"
+          color={loadColor ? "" :"#ffffff"}
+          ariaLabel="three-dots-loading"
+          wrapperStyle={{}}
+          wrapperClassName=""
+          visible={true}
+        />
+      ) : (
+        <span>{text}</span>
+      )}
+    </button>
+  );
+};
+
+export default Button;
