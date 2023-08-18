e9c94e617d2367cbb32b65072d59f013ce20d299
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2023-08-18 22:05:53 +0100

    Feat: multiple image push

--- src/components/primitives/shared/project-detail-accordion/ProjectDetailAccordion.jsx
+++ src/components/primitives/shared/project-detail-accordion/ProjectDetailAccordion.jsx
@@ -5,15 +5,6 @@ import getFileDetails from "utils/getFileDetails";
 
 const ProjectDetailAccordion = ({ data }) => {
   const [isExpanded, setIsExpanded] = useState(false);
-  const { image: imageAuth, name: nameAuth } = getFileDetails(
-    data?.letter_of_authorization_url
-  );
-  const { image: imageIdentity, name: nameIdentity } = getFileDetails(
-    data?.admin_identity_document_url
-  );
-  const { image: imageProjDoc, name: nameProjDoc } = getFileDetails(
-    data?.document_url
-  );
 
   const handleAccordianClick = () => {
     setIsExpanded(!isExpanded);
@@ -55,6 +46,8 @@ const ProjectDetailAccordion = ({ data }) => {
             <div className="project_details_accordion_item_wrap">
               {data?.document_urls?.map((item) => {
                 const { image, name } = getFileDetails(item);
+                const cleanedName = name.replace("%2F", "");
+
                 return (
                   <div className="project_details_accordion_item">
                     <div className="project_details_accordion_item_value_wrap">
@@ -68,7 +61,7 @@ const ProjectDetailAccordion = ({ data }) => {
                           className="project_details_accordion_item_value_sm"
                           href={data?.letter_of_authorization_url}
                         >
-                          {nameProjDoc || "Download"}
+                          {cleanedName || "Download"}
                         </a>
                         <span className="project_details_accordion_item_value_sm">
                           Download
@@ -82,50 +75,53 @@ const ProjectDetailAccordion = ({ data }) => {
           </>
         )}
         {data?.progress === "Phase6" && (
-          <div className="project_details_complete_wrap between">
-            <div className="project_details_complete_info">
-              <div className="text project_details_complete_info_text">
-                Project Documentation
-              </div>
-              <div className="project_details_complete_item_wrap">
-                {data?.document_urls?.map((item) => {
-                  const { image, name } = getFileDetails(item);
-                  return (
-                    <div className="project_details_accordion_item">
-                      <div className="project_details_accordion_item_value_wrap">
-                        <img
-                          src={image || name}
-                          alt="icon"
-                          className="project_details_accordion_item_img"
-                        />
-                        <div className="project_details_accordion_item_download">
-                          <a
-                            className="project_details_accordion_item_value_sm"
-                            href={data?.letter_of_authorization_url}
-                          >
-                            {nameProjDoc || "Download"}
-                          </a>
-                          <span className="project_details_accordion_item_value_sm">
-                            Download
-                          </span>
+          <>
+            <div className="text project_details_desc">
+              <div className="project_details_desc_head">Project Documents</div>
+            </div>
+            <div className="project_details_complete_wrap between">
+              <div className="project_details_complete_info">
+                <div className="project_details_complete_item_wrap">
+                  {data?.document_urls?.map((item) => {
+                    const { image, name } = getFileDetails(item);
+                    const cleanedName = name.replace("%2F", "");
+                    return (
+                      <div className="project_details_accordion_item">
+                        <div className="project_details_accordion_item_value_wrap">
+                          <img
+                            src={image || name}
+                            alt="icon"
+                            className="project_details_accordion_item_img"
+                          />
+                          <div className="project_details_accordion_item_download">
+                            <a
+                              className="project_details_accordion_item_value_sm"
+                              href={data?.letter_of_authorization_url}
+                            >
+                              {cleanedName || "Download"}
+                            </a>
+                            <span className="project_details_accordion_item_value_sm">
+                              Download
+                            </span>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  );
-                })}
+                    );
+                  })}
+                </div>
+              </div>
+              <div className="project_details_complete_cert">
+                <div className="text project_details_complete_cert_text">
+                  Project Certificate
+                </div>
+                <img
+                  src={certificate}
+                  alt="certificate"
+                  className="project_details_complete_cert_img"
+                />
               </div>
             </div>
-            <div className="project_details_complete_cert">
-              <div className="text project_details_complete_cert_text">
-                Project Certificate
-              </div>
-              <img
-                src={certificate}
-                alt="certificate"
-                className="project_details_complete_cert_img"
-              />
-            </div>
-          </div>
+          </>
         )}
       </div>
     </div>
