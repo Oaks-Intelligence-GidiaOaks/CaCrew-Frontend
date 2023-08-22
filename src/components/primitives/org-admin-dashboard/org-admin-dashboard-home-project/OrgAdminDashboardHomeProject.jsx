41700fdbb542bf3ef74862eb819f62208ba7287f
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2023-08-22 17:03:35 +0100

    Feat: (proj component)

--- src/components/primitives/org-admin-dashboard/org-admin-dashboard-home-project/OrgAdminDashboardHomeProject.jsx
+++ src/components/primitives/org-admin-dashboard/org-admin-dashboard-home-project/OrgAdminDashboardHomeProject.jsx
@@ -3,10 +3,14 @@ import "./OrgAdminDashboardHomeProject.scss";
 import { square } from "assets/images";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useAllProjectsQuery } from "services/project.service";
+import { Shimmer, TrackProjectProgress } from "components";
 
 const OrgAdminDashboardHomeProject = () => {
   const user = useSelector((state) => state.user.user);
-  console.log(user, "test");
+  const { data } = useAllProjectsQuery();
+  console.log(data, "test");
+
   return (
     <div className="org_dash_home_project">
       <div className="org_dash_home_project_heading">Projects</div>
@@ -16,18 +20,38 @@ const OrgAdminDashboardHomeProject = () => {
           <div className="org_dash_home_project_wrap_heading">Progress</div>
           <div className="org_dash_home_project_wrap_heading">View All</div>
         </div>
-        <div className="org_dash_home_project_empty center col">
-          <img src={square} alt="icon" />
-          <Link
-            to={user?.organization_id?.isVerified ? "/dashboard-project" : ""}
-            className={`org_dash_home_project_empty_text ${
-              !user?.organization_id?.isVerified &&
-              "org_dash_home_project_empty_notallowed"
-            }`}
-          >
-            Create a Project
-          </Link>
-        </div>
+        {data && data?.length < 1 ? (
+          <div className="org_dash_home_project_empty center col">
+            <img src={square} alt="icon" />
+            <Link
+              to={user?.organization_id?.isVerified ? "/dashboard-project" : ""}
+              className={`org_dash_home_project_empty_text ${
+                !user?.organization_id?.isVerified &&
+                "org_dash_home_project_empty_notallowed"
+              }`}
+            >
+              Create a Project
+            </Link>
+          </div>
+        ) : data?.length > 0 ? (
+          <div>
+            {data?.slice(0, 3)?.map((item) => (
+              <div className="org_dash_home_project_wrap_heading_item start" key={item?._id}>
+                <div className="org_dash_home_project_wrap_heading">{item?.project_name}</div>
+                <div className="org_dash_home_project_wrap_heading">
+                  <TrackProjectProgress phase={item?.progress}/>
+                  Phase {item?.progress[item?.progress?.length - 1]}
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div>
+            {Array(3).map(() => (
+              <Shimmer height={"40px"} />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
--- src/components/primitives/staff-dashboard/staff-dashboard-banner/StaffDashBanner.jsx
+++ src/components/primitives/staff-dashboard/staff-dashboard-banner/StaffDashBanner.jsx
@@ -1,19 +1,35 @@
-import React from 'react'
-import "./StaffDashBanner.scss"
-import { useGetUserQuery } from 'services/user.service'
-import { avartar } from 'assets/images'
+import React from "react";
+import "./StaffDashBanner.scss";
+import { useGetUserQuery } from "services/user.service";
+import { avartar } from "assets/images";
+import { Shimmer } from "components";
 
 const StaffDashBanner = () => {
-  const {data: user} = useGetUserQuery()
+  const { data: user } = useGetUserQuery();
   return (
-    <div className='staff_banner between'>
-      <img src={user?.photo_url || avartar} alt="avatar" className='staff_banner_img'/>
-      <div>
-        <di>Organization</di>
-        <di>{user?.organization_id?.organization_name}</di>
+    <div className="staff_banner between">
+      <div className="staff_banner_name_wrap start">
+        <img
+          src={user?.photo_url || avartar}
+          alt="avatar"
+          className="staff_banner_img"
+        />
+        <div className="staff_banner_name">
+          {user ? user?.name : <Shimmer height={"20px"} width={"100px"} />}
+        </div>
+      </div>
+      <div className="staff_banner_title_wrap">
+        <div className="text staff_banner_title">Organization</div>
+        <div className="staff_banner_val ">
+          {user ? (
+            user?.organization_id?.organization_name
+          ) : (
+            <Shimmer height={"20px"} width={"100px"} />
+          )}
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default StaffDashBanner
+export default StaffDashBanner;
