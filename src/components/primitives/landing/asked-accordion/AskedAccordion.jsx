d24da3cee9183a4cb4f23574bc8aa67514282b1b
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2023-08-26 13:01:20 +0100

    Chore: (Animate)

--- src/components/primitives/landing/asked-accordion/AskedAccordion.jsx
+++ src/components/primitives/landing/asked-accordion/AskedAccordion.jsx
@@ -5,9 +5,9 @@ import { down } from "assets/images";
 const AskedAccordion = ({ title, text }) => {
   return (
     <div className="asked_accordion">
-      <div className="asked_accordion_title_wrap">
+      <div className="asked_accordion_title_wrap between">
         <div className="asked_accordion_title">{title}</div>
-        <div className="asked_accordion_img_wrap">
+        <div className="asked_accordion_img_wrap center">
           <img src={down} alt="icon" className="asked_accordion_img" />
         </div>
       </div>
