ee20ee23835c4141932c1c7608f3372193e973c6
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2023-09-04 16:49:41 +0100

    Chore: (scroll effect mod)

--- src/pages/shared/RegistryDetailPage.jsx
+++ src/pages/shared/RegistryDetailPage.jsx
@@ -1,10 +1,13 @@
+import { LandingFooter, LandingHeader } from "components";
 import { Dashboard, RegistryDetail } from "containers";
 import React from "react";
 
 const RegistryDetailPage = () => {
   return (
     <>
+      <LandingHeader />
       <RegistryDetail />
+      <LandingFooter />
     </>
   );
 };
